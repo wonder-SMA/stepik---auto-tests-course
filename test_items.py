23eec872c969f6d1294556d194e18ff1e9499c70
wonder-SMA <61292853+wonder-SMA@users.noreply.github.com>
2020-05-13 20:55:53 +0400

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -3,7 +3,7 @@ import pytest, time
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-def test_button(browser):
+def test_add_to_basket(browser):
     browser.get(link)
     time.sleep(30)
     button = browser.find_element_by_css_selector("button.btn-add-to-basket")
